9daba95752453445cc64be65f3ad1212dede55c7
Felipe  Rojas <f0r01eo@M-C02D8E40MD6M.local>
2023-09-25 23:23:09 -0300

    python code with model

--- new_or_used.py
+++ new_or_used.py
@@ -24,6 +24,16 @@ The deliverables are:
 """
 
 import json
+import re
+import pandas as pd
+from pandas import json_normalize
+import numpy as np
+import os
+import datetime
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
 
 
 # You can safely assume that `build_dataset` is correctly implemented
@@ -39,6 +49,64 @@ def build_dataset():
         del x["condition"]
     return X_train, y_train, X_test, y_test
 
+def pre_process(text):
+    ''' funcion que elimina caracteres especiales en columnas '''
+    text = str(text)                             # Converting texto into string
+    text = re.sub(r'[^\w\s]', ' ', text)         # Remove all the special characters
+    return text
+
+def fechas(df, column_to_look):
+    ''' Agrupa y cuenta los datos en diferentes periodos y los agrega al dataframe original '''
+    column_to_look = str(column_to_look)
+    df['datetime'] = pd.to_datetime(df[column_to_look],format='%Y-%m-%d').dt.date
+    df['dia'] = pd.to_datetime(df[column_to_look], dayfirst=True).dt.day.astype('int')
+    df['semana'] = pd.to_datetime(df[column_to_look], dayfirst=True).dt.isocalendar().week
+    df['mes'] = pd.to_datetime(df[column_to_look], dayfirst=True).dt.month.astype('int')
+    df['ano'] = pd.to_datetime(df[column_to_look], dayfirst=True).dt.year.astype('int')
+    return df
+
+def clean_flattern_json(df):
+    ''' Función que aplana y limpia los datos aplicando la función de json_normalize y pre_process '''
+    #1st normalization: flatten columns with json files
+    dfp = json_normalize(df)
+    #2nd normalization: flatten nested columns with json files
+    nested_columns = ['non_mercado_pago_payment_methods', 'pictures']
+    for col in nested_columns:
+        df_col = json_normalize(df, record_path=col)
+        columns_name = df_col.columns
+        for ncols in columns_name:
+            dfp[col+'.'+ncols] = df_col[ncols]
+    #remove nested columns 
+    dfp.drop(nested_columns, axis='columns', inplace=True)
+    #3rd normalization: remove special characters from columns
+    columns_with_special_characters = ['sub_status',
+                                       'deal_ids',
+                                       'variations',
+                                       'attributes',
+                                       'tags',
+                                       'coverage_areas',
+                                       'descriptions',
+                                       'shipping.methods',
+                                       'shipping.tags']
+    for col in columns_with_special_characters:
+        dfp[col] = dfp[col].apply(pre_process)
+    #4th convert boolean columns into strings
+    boolean_columns = ['accepts_mercadopago','automatic_relist','shipping.local_pick_up','shipping.free_shipping']
+    for col in boolean_columns:
+        dfp[col] = dfp[col].map({True: 'True', False: 'False'}) 
+    return dfp
+
+def select_columns(numerical_columns, categorical_columns, X_train_df, X_test_df):
+        X_full_df = pd.concat([X_train_df, X_test_df], axis=0)
+        X_full_num = X_full_df[numerical_columns]
+        X_full_cat = X_full_df[categorical_columns]
+        return X_full_num, X_full_cat
+
+def vectorize_categorical_columns(X_full_cat):
+        vectorizer = OneHotEncoder()
+        X_full_categorical = vectorizer.fit_transform(X_full_cat).toarray()
+        return X_full_categorical
+
 
 if __name__ == "__main__":
     print("Loading dataset...")
@@ -50,6 +118,54 @@ if __name__ == "__main__":
     X_train, y_train, X_test, y_test = build_dataset()
 
     # Insert your code below this line:
-    # ...
+    
+    # Prepare and clean data
+    print('Cleaning data! ')
+    X_train_df = clean_flattern_json(X_train)    
+    X_test_df = clean_flattern_json(X_test)
+    
+    # Select Columns to Train
+    numerical_columns = ['stop_time',
+                         'start_time',
+                         'available_quantity',
+                         'price',
+                         'initial_quantity',
+                         'sold_quantity']
+    
+    categorical_columns = ['listing_type_id', 
+                           'buying_mode', 
+                           'accepts_mercadopago', 
+                           'currency_id', 
+                           'automatic_relist', 
+                           'status',
+                           'seller_address.state.name',
+                           'seller_address.city.name',
+                           'shipping.local_pick_up', 
+                           'shipping.free_shipping', 
+                           'shipping.mode',
+                           'non_mercado_pago_payment_methods.description',
+                           'non_mercado_pago_payment_methods.type']
+    
+    print('Selecting features to train! ')
+    X_full_num, X_full_cat = select_columns(numerical_columns, categorical_columns, X_train_df, X_test_df)
+    
+    print('Vectorizing categorical features! ')
+    X_full_categorical = vectorize_categorical_columns(X_full_cat)
+    X_full_numerical = X_full_num.to_numpy()
+    X_full = np.concatenate((X_full_numerical, X_full_categorical), axis=1)
 
+    print('Preparing data! ')
+    X_train = X_full[:-10000]
+    X_test = X_full[-10000:]
 
+    print('Trainig model! This section can take several minutes')
+    rf_model = RandomForestClassifier(n_estimators=300, random_state=0)
+    rf_model.fit(X_train, y_train)
+    y_pred = rf_model.predict(X_test)
+
+    print('Classification Report!')
+    print(classification_report(y_test, y_pred))
+    accuracy = 100*accuracy_score(y_test, y_pred)
+    print('Accuracy: {:.2f}%'.format(accuracy))
+    
+    print('\nEnd of the Code! ')
